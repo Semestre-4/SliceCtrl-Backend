037cb8283ae9ceb3463101776f7a931b72b3eff7
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-10 20:06:41 -0300

    first commit

---
+++ src/main/java/com/mensal/sliceCtrl/SliceCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.mensal.sliceCtrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliceCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliceCtrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mensal/sliceCtrl/SliceCtrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensal.sliceCtrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliceCtrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
