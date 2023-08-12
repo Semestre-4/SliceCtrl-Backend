a5a0a0965c17b734171c1b2b60fcb5cf66306b50
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-08-12 11:44:48 -0300

    Atualizado Enderecos entity

--- src/main/java/com/mensal/sliceCtrl/entity/Enderecos.java
+++ src/main/java/com/mensal/sliceCtrl/entity/Enderecos.java
@@ -3,6 +3,7 @@ package com.mensal.sliceCtrl.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -24,11 +25,17 @@ public class Enderecos extends AbstractEntity {
     @NotNull(message = "É obrigatorio informar o bairro")
     @Column(name = "bairro", nullable = false)
     @Size(min = 2, max = 20, message = "O bairro pode ter entre 2 e 20 caracteres")
-    String Bairro;
+    String bairro;
+
+    @NotNull(message = "É obrigatorio informar a cidade")
+    @NotBlank(message = "É obrigatorio informar a cidade")
+    @Column(name = "cidade", nullable = false)
+    @Size(min = 3, max = 50, message = "A cidade pode ter entre 3 e 50 caracteres")
+    String cidade;
 
     @NotNull(message = "É obrigatorio informar o CEP")
     @Column(name = "cep", nullable = false)
     @Size(min = 7, max = 11, message = "CEP Informado incorretamente")
-    String CEP;
+    String cep;
 
 }
