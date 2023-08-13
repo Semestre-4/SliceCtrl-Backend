99bb61855fca09ca8bc42837a697468e5c4e258e
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-12 22:48:59 -0300

    DTO | FuncionarioDTO.java created.

---
+++ src/main/java/com/mensal/sliceCtrl/DTO/FuncionarioDTO.java
@@ -0,0 +1,32 @@
+package com.mensal.sliceCtrl.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.math.BigDecimal;
+
+@Data
+public class FuncionarioDTO {
+
+    @NotBlank(message = "O nome do funcionario é obrigatório")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    @NotNull(message = "O campo 'nome' não pode ser nulo")
+    private String nome;
+
+    @CPF(message = "O CPF é inválido")
+    @NotBlank(message = "O CPF do funcionario é obrigatório")
+    @NotNull(message = "O campo 'CPF' não pode ser nulo")
+    private String cpf;
+
+    @NotBlank(message = "O telefone do funcionario é obrigatório")
+    @NotNull(message = "O campo 'telefone' não pode ser nulo")
+    private String telefone;
+
+    @NotNull(message = "O campo 'salario' não pode ser nulo")
+    private BigDecimal salario;
+
+
+}
