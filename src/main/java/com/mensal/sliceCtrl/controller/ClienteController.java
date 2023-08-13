d4777d27860b967f1a913690faed04f63ae34e4c
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-13 12:00:43 -0300

    cliente | ClienteController.java + ClienteRepository initialized .

---
+++ src/main/java/com/mensal/sliceCtrl/controller/ClienteController.java
@@ -0,0 +1,80 @@
+package com.mensal.sliceCtrl.controller;
+
+import com.mensal.sliceCtrl.DTO.ClienteDTO;
+import com.mensal.sliceCtrl.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDTO> getClienteById(@PathVariable("id") Long id) {
+        ClienteDTO cliente = clienteService.findById(id);
+        if (cliente != null) {
+            return ResponseEntity.ok(cliente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<List<ClienteDTO>> getClientesByNome(@PathVariable("nome") String nome) {
+        List<ClienteDTO> clientes = clienteService.findByNome(nome);
+        if (!clientes.isEmpty()) {
+            return ResponseEntity.ok(clientes);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<List<ClienteDTO>> getClientesByCPF(@PathVariable("cpf") String cpf) {
+        List<ClienteDTO> clientes = clienteService.findByCPF(cpf);
+        if (!clientes.isEmpty()) {
+            return ResponseEntity.ok(clientes);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ClienteDTO>> getAllClientes() {
+        List<ClienteDTO> clientes = clienteService.findAll();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> postCliente(@RequestBody ClienteDTO clienteDTO) {
+        ClienteDTO createdCliente = clienteService.createCliente(clienteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteDTO> putCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
+        ClienteDTO updatedCliente = clienteService.updateCliente(id, clienteDTO);
+        if (updatedCliente != null) {
+            return ResponseEntity.ok(updatedCliente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCliente(@PathVariable Long id) {
+        try {
+            clienteService.deleteCliente(id);
+            return ResponseEntity.ok().body("Cliente excluido com sucesso!");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/mensal/sliceCtrl/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.mensal.sliceCtrl.repository;
+
+import com.mensal.sliceCtrl.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+}
