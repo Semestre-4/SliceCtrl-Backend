b10197a4755ab28edb8d99547a7f631e8976811b
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-12 22:46:20 -0300

    DTO | ClienteDTO.java created.

---
+++ src/main/java/com/mensal/sliceCtrl/DTO/ClienteDTO.java
@@ -0,0 +1,36 @@
+package com.mensal.sliceCtrl.DTO;
+
+import com.mensal.sliceCtrl.entity.Enderecos;
+import com.mensal.sliceCtrl.entity.Pedido;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.List;
+
+public class ClienteDTO {
+
+    @NotBlank(message = "O nome do cliente é obrigatório")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    @NotNull(message = "O campo 'nome' não pode ser nulo")
+    private String nome;
+
+    @CPF(message = "O CPF é inválido")
+    @NotBlank(message = "O CPF do cliente é obrigatório")
+    @NotNull(message = "O campo 'CPF' não pode ser nulo")
+    private String cpf;
+
+    @NotBlank(message = "O telefone do cliente é obrigatório")
+    @NotNull(message = "O campo 'telefone' não pode ser nulo")
+    private String telefone;
+
+    @Email(message = "O email é inválido")
+    private String email;
+
+    private List<Enderecos> enderecos;
+
+    private List<Pedido> pedidos;
+
+}
