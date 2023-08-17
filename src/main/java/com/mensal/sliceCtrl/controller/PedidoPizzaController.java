5332fe0e669f3ae7b76933fd42ffabba8f378999
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-17 00:37:51 -0300

    Controllers of new entities created.

---
+++ src/main/java/com/mensal/sliceCtrl/controller/PedidoPizzaController.java
@@ -0,0 +1,9 @@
+package com.mensal.sliceCtrl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/pedido/pizza")
+public class PedidoPizzaController {
+}
