f24c3f9de81cdc30e738af65c5c1b45fd64555cc
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-17 00:29:21 -0300

    DTO's of PedidoPizzaDTO e PedidoProdutoDTO created.

---
+++ src/main/java/com/mensal/sliceCtrl/DTO/PedidoPizzaDTO.java
@@ -0,0 +1,11 @@
+package com.mensal.sliceCtrl.DTO;
+
+public class PedidoPizzaDTO extends AbstractEntityDTO{
+
+    private PizzasDTO pizza;
+
+    private PedidosDTO pedido;
+
+    private int qtdePedida;
+
+}
---
+++ src/main/java/com/mensal/sliceCtrl/DTO/PedidoProdutoDTO.java
@@ -0,0 +1,9 @@
+package com.mensal.sliceCtrl.DTO;
+
+public class PedidoProdutoDTO extends AbstractEntityDTO {
+
+    private ProdutosDTO produto;
+    private PedidosDTO pedido;
+    private int qtdePedida;
+
+}
