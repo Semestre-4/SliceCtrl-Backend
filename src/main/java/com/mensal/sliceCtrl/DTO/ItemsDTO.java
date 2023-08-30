6dc62dfe8312c42109f7550b614a7a4186ac1403
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-29 23:42:22 -0300

    ItemDTO created to display data in frontend.

---
+++ src/main/java/com/mensal/sliceCtrl/DTO/ItemsDTO.java
@@ -0,0 +1,11 @@
+package com.mensal.sliceCtrl.DTO;
+
+import com.mensal.sliceCtrl.entity.Pizzas;
+import com.mensal.sliceCtrl.entity.Produtos;
+
+import java.util.List;
+
+public class ItemsDTO {
+    List<Produtos> produtos;
+    List<Pizzas> pizzas;
+}
