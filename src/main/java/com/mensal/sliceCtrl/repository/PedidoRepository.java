14e4f5c7c54dbe0ac902fd79ff3c54c1167195bc
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-07 18:11:38 -0300

    PedidoRepository.findByformaDeEntrega added.

--- src/main/java/com/mensal/sliceCtrl/repository/PedidoRepository.java
+++ src/main/java/com/mensal/sliceCtrl/repository/PedidoRepository.java
@@ -1,17 +1,22 @@
 package com.mensal.sliceCtrl.repository;
 
 import com.mensal.sliceCtrl.entity.Pedidos;
+import com.mensal.sliceCtrl.entity.enums.FormaDeEntrega;
 import com.mensal.sliceCtrl.entity.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedidos, Long> {
 
     @Query("SELECT p FROM Pedidos p WHERE p.status = :status")
     List<Pedidos> findByStatus(@Param("status") Status status);
+
+    @Query("SELECT p FROM Pedidos p WHERE p.formaDeEntrega = :formaDeEntrega")
+    List<Pedidos> findByformaDeEntrega(FormaDeEntrega formaDeEntrega);
 
     @Query("SELECT p FROM Pedidos p WHERE p.cliente.id = :id")
     List<Pedidos> findByCliente(@Param("id") Long id);
@@ -21,4 +26,5 @@ public interface PedidoRepository extends JpaRepository<Pedidos, Long> {
 
     @Query("SELECT p FROM Pedidos p WHERE p.status = :PENDENTE")
     List<Pedidos> findOrdersWithPendingPayments();
+
 }
