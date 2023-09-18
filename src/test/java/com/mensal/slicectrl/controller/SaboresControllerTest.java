6586b8759f77394754d5dc3dce004657242dd72a
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-09-18 19:41:26 -0300

    feat: Criado teste de getById de sabores.

---
+++ src/test/java/com/mensal/slicectrl/controller/SaboresControllerTest.java
@@ -0,0 +1,49 @@
+package com.mensal.slicectrl.controller;
+
+import com.mensal.slicectrl.dto.ProdutosDTO;
+import com.mensal.slicectrl.dto.SaboresDTO;
+import com.mensal.slicectrl.service.ProdutoService;
+import com.mensal.slicectrl.service.SaboresService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class SaboresControllerTest {
+
+    @InjectMocks
+    private SaboresController controller;
+
+    @Mock
+    private SaboresService service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetByIdSabores() {
+        Long saboresId = 1L;
+        SaboresDTO saboresDTO = new SaboresDTO();
+
+        Mockito.when(service.getById(saboresId)).thenReturn(saboresDTO);
+
+        ResponseEntity<SaboresDTO> response = controller.getById(saboresId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(saboresDTO, response.getBody());
+
+        verify(service, times(1)).getById(saboresId);
+    }
+
+
+}
