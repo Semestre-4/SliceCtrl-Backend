f933c994f303426c617ddab0ebae23b6e33cbc70
Bouchra Akl <bushraakl1234@gmail.com>
2023-11-10 21:48:38 -0300

    feat : initial spring config

--- src/main/java/com/mensal/slicectrl/entity/Usuario.java
+++ src/main/java/com/mensal/slicectrl/entity/Usuario.java
@@ -1,15 +1,19 @@
 package com.mensal.slicectrl.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mensal.slicectrl.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "usuario",schema = "public")
@@ -32,14 +36,22 @@ public class Usuario extends AbstractEntity implements UserDetails {
     @Column(name = "salario")
     private BigDecimal salario;
 
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "usuario_roles", joinColumns = @JoinColumn(name = "usuario_id"))
+    private Set<Role> roles;
+
     @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Pedidos> pedidos;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
     }
+
 
     @Override
     public String getUsername() {
