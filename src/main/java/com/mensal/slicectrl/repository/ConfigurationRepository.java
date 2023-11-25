72c4dfcfea56ca48e3ef9a71d37c4dd08a2762c3
Bouchra Akl <bushraakl1234@gmail.com>
2023-11-25 10:54:08 -0300

    delete : configuration take back.

--- src/main/java/com/mensal/slicectrl/repository/ConfigurationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mensal.slicectrl.repository;
-
-import com.mensal.slicectrl.entity.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfigurationRepository extends JpaRepository<Configuration,Long> {
-}
