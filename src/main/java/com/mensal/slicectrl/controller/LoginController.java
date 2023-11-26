9301c9de31234ea22d6ce8fe8f5a4bea89512bc0
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-26 16:23:20 -0300

    Atualizado spring security

--- src/main/java/com/mensal/slicectrl/controller/LoginController.java
+++ src/main/java/com/mensal/slicectrl/controller/LoginController.java
@@ -12,26 +12,30 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/login")
-public class LoginController {
+@CrossOrigin("http://localhost:4200")
+public class LogInController {
 
-    @Autowired
-    private LoginService loginService;
+    @Autowired  private LoginService loginService;
 
-    @PostMapping
-    public ResponseEntity<UsuarioDTO> logar(@RequestBody LoginDTO loginDTO) {
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody LoginDTO loginDTO) {
         try {
-            return ResponseEntity.ok(loginService.logar(loginDTO));
-        }catch(AuthenticationException ex) {
-            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
-        }catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            UsuarioDTO authenticatedUser = loginService.authenticate(loginDTO);
+            return ResponseEntity.ok(authenticatedUser);
+        } catch (AuthenticationException ex) {
+            return new ResponseEntity<>("Authentication failed: " + ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Bad request: " + e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping("deslogar")
+
+    @GetMapping("/logout")
     public ResponseEntity<HttpStatus> logout() {
         SecurityContextHolder.clearContext();
         return new ResponseEntity<>(null, HttpStatus.OK);
-    }
-}
 
+    }
+
+
+}
